07e02a7f9a41e0404f869bd1915e4d5de593facb
YujiaGeger <168009081+YujiaGeger@users.noreply.github.com>
2025-01-02 15:51:41 -0500

    Update MCUlog_parser.py

--- MCUlog_parser.py
+++ MCUlog_parser.py
@@ -2,7 +2,8 @@ import os
 import re
 import sys
 from datetime import datetime, timedelta
-#xin de donng xi
+def abc(v):
+    return v + 30
 def read_logs(mcu_log_path, error_time):
     """
     读取日志文件并提取在指定时间之前的ErrorManager错误日志。
