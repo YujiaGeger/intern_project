ccb83f975e2b7fa1f6898a3c6bcf75fb835edc41
YujiaGeger <168009081+YujiaGeger@users.noreply.github.com>
2025-01-02 15:41:02 -0500

    Delete Unnecessry Lines
    BA;A=HDSGFJHDSG

--- MCUlog_parser.py
+++ MCUlog_parser.py
@@ -2,8 +2,6 @@ import os
 import re
 import sys
 from datetime import datetime, timedelta
-#fjsdhfjkdshfkjshkj
-#hjkfhsdjfhdsjkf
 def read_logs(mcu_log_path, error_time):
     """
     读取日志文件并提取在指定时间之前的ErrorManager错误日志。
